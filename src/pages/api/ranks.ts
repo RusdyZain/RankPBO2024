60b83c9b90eba12675a279556f4696ba3718146d
RusdyZain <ryanfajri28@gmail.com>
2024-11-26 06:13:31 +0800

    Push

--- src/pages/api/ranks.ts
+++ src/pages/api/ranks.ts
@@ -1,14 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
-const ranks = [
-  { nim: "12345", rank: 1 },
-  { nim: "67890", rank: 2 },
-];
+import data from "../../data/ranks.json";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { nim } = req.query;
 
-  const result = ranks.find((student) => student.nim === nim);
+  const result = data.find((student) => student.NIM === nim);
+
   if (!result) {
     return res.status(404).json({ error: "Rank not found" });
   }
